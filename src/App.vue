7a6070e2078ac0ad64813317d42ab3b350a6f19c
s0loparty <izovo@bk.ru>
2021-12-09 13:25:36 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@
 	<router-view></router-view>
 </template>
 
-<script>
-import TheHeader from "@/components/TheHeader.vue"
-
-export default {
-	components: { TheHeader }
-}
+<script setup>
+	import TheHeader from "@/components/TheHeader.vue"
 </script>
