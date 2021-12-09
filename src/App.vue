494490f5a99ceecbf69f77c5dfc8a872869602cf
s0loparty <izovo@bk.ru>
2021-12-09 17:54:34 +0300

    style & dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
 	<the-header></the-header>
 	<router-view></router-view>
+	<TheToggleTheme></TheToggleTheme>
 </template>
 
 <script setup>
 	import TheHeader from "@/components/TheHeader.vue"
+	import TheToggleTheme from "./components/TheToggleTheme.vue";
 </script>
