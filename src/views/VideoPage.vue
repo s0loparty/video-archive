54d53b22ab678707ca59e6c4332ef94f604265ba
s0loparty <izovo@bk.ru>
2021-12-13 15:30:31 +0300

    Update VideoPage.vue
    fix dynamic video player

--- src/views/VideoPage.vue
+++ src/views/VideoPage.vue
@@ -4,9 +4,7 @@
 			<div class="vpage__wrap">
 				<div class="vpage__content">
 					<div class="vpage__player-wrap" :style="`background-image: url(${video.preview});`">
-						<!-- <iframe :src="video.source" frameborder="0" allowfullscreen class="vpage__player"></iframe> -->
-
-						<video ref="test" id="player" playsinline controls :data-poster="video.preview">
+						<video id="player" playsinline controls>
 							<source :src="video.source" type="video/mp4" />
 						</video>
 					</div>
@@ -31,7 +29,7 @@
 </template>
 
 <script setup>
-	import { computed, ref, onMounted } from "vue"
+	import { computed, ref, onMounted, watch } from "vue"
 	import { useRoute } from "vue-router"
 	import { useStore } from 'vuex'
 
@@ -48,14 +46,23 @@
 	const allVideos = store.getters.getVideos
 	const history = store.getters['history/getHistory']
 
-	const videoPlayer = ref(null)
-	onMounted(() => {
-		videoPlayer.value = new Plyr(document.getElementById('player'), {
-			tooltips: { controls: true, seek: true },
-			title: video.value.title,
-			blankVideo: video.value.source,
-			urls: { download: video.value.source },
-		})
+	const plyrObj = ref(null)
+	const initPlyr = (videoValue = null) => {
+		videoValue = videoValue ?? video.value
+		plyrObj.value = new Plyr('#player')
+		plyrObj.value.source = {
+			type: 'video',
+			title: videoValue.title,
+			sources: [{ src: videoValue.source, src: videoValue.source }],
+			poster: videoValue.preview
+		}
+	}
+	onMounted(initPlyr)
+	watch(video, (newVideoValue, o) => {
+		if (newVideoValue) {
+			plyrObj.value.destroy()
+			initPlyr(newVideoValue)
+		}
 	})
 
 	const historyVideos = history.map(item => allVideos.find(v => v.id == item))
